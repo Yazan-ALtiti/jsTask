a347810dac5e99e319a2056d12023a58739389a7
Yazan-ALtiti <titiyazan14@gmail.com>
2024-07-17 08:00:26 +0300

    update

--- secript.js
+++ secript.js
@@ -1,1 +1,11 @@
-console.log ("Yazan");
+/*
+let userInput =prompt("What is your name: ");
+
+alert(`Welcoum ${userInput} enjoy`);
+
+//confirm H.W
+*/
+
+
+let userInput = prompt("What is your name: ");
+alert(`Welcoum ${userInput} enjoy`);
