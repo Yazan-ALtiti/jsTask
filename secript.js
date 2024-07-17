f3a05d0ac4749d3af3afe8c2903d7a94698350db
Yazan-ALtiti <titiyazan14@gmail.com>
2024-07-17 08:12:36 +0300

    update

--- secript.js
+++ secript.js
@@ -1,11 +1,9 @@
-/*
-let userInput =prompt("What is your name: ");
 
-alert(`Welcoum ${userInput} enjoy`);
-
-//confirm H.W
-*/
 
 
 let userInput = prompt("What is your name: ");
 alert(`Welcoum ${userInput} enjoy`);
+
+
+let confirmMsg=confirm("Are you sure?");
+console.log(confirmMsg);
